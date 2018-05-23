d8111615816451930e20ac52156fdc859130a508
Kenny Lehr <kenman9009@gmail.com>
2018-05-23 13:14:42 -0400

    pre migration

--- app/controllers/concerns/api/v1/characters_controller.rb
+++ app/controllers/concerns/api/v1/characters_controller.rb
@@ -7,7 +7,6 @@ class Api::V1::CharactersController < ApplicationController
 
   def create
     character = Character.create(name: params[:name], user_id: params[:user_id], campaign_id: params[:campaign_id])
-    byebug
     render json: character
   end
 
